fad473c94a6f3e24e6259120f83d933cf149368e
nknighta <n.knight.pc0627@gmail.com>
2024-04-19 17:45:45 +0900

    ✨ : feat - sconfig for vuejs project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    障がい者向け救援システム(仮)
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">ホーム</RouterLink>
+        <RouterLink to="/about">設定</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+  (c) cid buisiness project
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>設定</h1>
+    <button>aa</button>
+  </div>
+</template>
+
+<style>
+</style>
